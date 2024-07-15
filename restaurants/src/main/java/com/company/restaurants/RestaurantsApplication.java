845fab57a01c1a8d27705bcd3aee31d2ac7c25e8
Cooking <Cooking@host.docker.internal>
2024-07-15 18:22:55 +0900

    소스 mapper 스캔 / DB 연동

--- restaurants/src/main/java/com/company/restaurants/RestaurantsApplication.java
+++ restaurants/src/main/java/com/company/restaurants/RestaurantsApplication.java
@@ -1,9 +1,11 @@
 package com.company.restaurants;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.company.restaurants.*")
 public class RestaurantsApplication {
 
 	public static void main(String[] args) {
--- restaurants/src/main/java/com/company/restaurants/review/mapper/reviewMapper.java
+++ restaurants/src/main/java/com/company/restaurants/review/mapper/reviewMapper.java
@@ -1,12 +1,13 @@
 package com.company.restaurants.review.mapper;
 
 import java.util.List;
-import java.util.Map;
+
+import org.springframework.stereotype.Repository;
 
 import com.company.restaurants.review.domain.reviewDTO;
 import com.company.restaurants.search.domain.restaurantsDTO;
 
-
+@Repository
 public interface reviewMapper {
 	
 	public List<reviewDTO> restaurantsReviewList(restaurantsDTO restaurantsDTO);
